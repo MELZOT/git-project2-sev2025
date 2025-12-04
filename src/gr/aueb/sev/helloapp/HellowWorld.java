ce0554bd2dd772cc378f0e0748946f374c4f7153
mzotika <mzotika@yahoo.gr>
2025-12-04 12:42:31 +0200

    add printf to say Hallo

--- src/gr/aueb/sev/helloapp/HellowWorld.java
+++ src/gr/aueb/sev/helloapp/HellowWorld.java
@@ -3,5 +3,6 @@ package gr.aueb.sev.helloapp;
 public class HellowWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.printf("Hello! %s", "World!");
     }
 }
